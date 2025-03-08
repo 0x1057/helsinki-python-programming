3d765ec0946324f947d49f78605326d6ccff7f63
0x1057 <0x1057.main@gmail.com>
2025-03-08 10:33:50 -0600

    First half of Part 1 of the  University of Helsinki's Introduction to Programming with Python Course

---
+++ tmcdata/mooc-programming-25/part01-08_name_and_address/src/name_and_address.py
@@ -0,0 +1,7 @@
+given = input("Given name:")
+last = input("Family name:")
+street = input("Street address:")
+postal = input("City and postal code:")
+print(f"{given} {last}")
+print(f"{street}")
+print(f"{postal}")
---
+++ tmcdata/mooc-programming-25/part01-15_seconds_in_a_day/src/seconds_in_a_day.py
@@ -0,0 +1,3 @@
+# Write your solution here
+days = int(input("How amny days?"))
+print(f"Seconds in that many days: {days * 24 * 60 * 60}")
